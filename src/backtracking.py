d3197939c337482eb91fbf94e7e1c768700bf368
Ritika Singh <ritika.singh2024@vitstudent.ac.in>
2026-04-09 00:01:20 +0530

    Day 4: Implemented backtracking graph coloring

---
+++ src/backtracking.py
@@ -0,0 +1,53 @@
+from graph import build_conflict_graph
+
+def is_safe(node, graph, color, assignment):
+    for neighbor in graph[node]:
+        if neighbor in assignment and assignment[neighbor] == color:
+            return False
+    return True
+
+
+def backtracking_coloring(graph, nodes, assignment, max_colors):
+    if len(assignment) == len(nodes):
+        return True
+
+    node = nodes[len(assignment)]
+
+    for color in range(max_colors):
+        if is_safe(node, graph, color, assignment):
+            assignment[node] = color
+
+            if backtracking_coloring(graph, nodes, assignment, max_colors):
+                return True
+
+            del assignment[node]
+
+    return False
+
+
+def solve_graph_coloring(graph):
+    nodes = list(graph.keys())
+
+    for max_colors in range(1, len(nodes) + 1):
+        assignment = {}
+        if backtracking_coloring(graph, nodes, assignment, max_colors):
+            return assignment
+
+    return None
+
+
+# Test
+if __name__ == "__main__":
+    enrollments={
+        "S1": ["C1", "C2"],
+        "S2": ["C1", "C3"],
+        "S3": ["C2", "C3"],
+        "S4": ["C2", "C4"]
+    }
+
+    graph = build_conflict_graph(enrollments)
+    result = solve_graph_coloring(graph)
+
+    print("Optimal Coloring:")
+    for course in result:
+        print(course, ":", result[course])
