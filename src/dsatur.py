2c6234e52911cea2f26c738a8fd97ab994fbe6ec
Ritika Singh <ritika.singh2024@vitstudent.ac.in>
2026-04-10 00:53:36 +0530

    Day 5: Implemented DSATUR graph coloring

---
+++ src/dsatur.py
@@ -0,0 +1,43 @@
+from graph import build_conflict_graph
+def dsatur_coloring(graph):
+    colors={}
+    saturation={node: 0 for node in graph}
+    degrees={node: len(graph[node]) for node in graph}
+    while len(colors)<len(graph):
+        #Selection of node with highest saturation
+        uncolored=[node for node in graph if node not in colors]
+        node=max(uncolored, key=lambda x: (saturation[x], degrees[x]))
+
+        #Find used colors in neighbours
+        used_color=set()
+        for neighbour in graph[node]:
+            if neighbour in colors:
+                used_color.add(colors[neighbour])
+        
+        #Assigning smallest available color
+        color=0
+        while color in used_color:
+            color+=1
+        colors[node]=color
+        #Update saturation of neighbours
+        for neighbour in graph[node]:
+            if neighbour not in colors:
+                neighbour_colors=set(colors[n] for n in graph[neighbour] if n in colors)
+                saturation[neighbour]=len(neighbour_colors)
+    
+    return colors
+
+#Test
+if __name__=="__main__":
+    enrollments={
+        "S1": ["C1", "C2"],
+        "S2": ["C1", "C3"],
+        "S3": ["C2", "C3"],
+        "S4": ["C2", "C4"]
+    }
+    graph=build_conflict_graph(enrollments)
+    result=dsatur_coloring(graph)
+
+    print("DSATUR Coloring:")
+    for course in result:
+        print(course, ":", result[course])
